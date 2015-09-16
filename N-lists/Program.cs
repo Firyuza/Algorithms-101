451c344fbfd6bfb6ac7d0c405ad9fdaf4e81a01d
Firyuza Shigapova <shigapovafz@gmail.com>
2015-09-17 00:31:37 +0300

    N-List program

---
+++ N-lists/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace N_lists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter number");
+
+            var n = int.Parse(Console.ReadLine());
+
+            NList list = new NList(n);
+
+            Console.WriteLine("Result");
+
+            list.ShowNList();
+
+            Console.ReadKey();
+        }
+    }
+}
