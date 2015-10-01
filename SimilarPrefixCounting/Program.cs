5da57df0273b042225f3f5c8f333dd6cd817c59e
Firyuza Shigapova <shigapovafz@gmail.com>
2015-10-01 13:05:02 +0300

    Trie algorithm

---
+++ SimilarPrefixCounting/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimilarPrefixCounting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TrieST prefixes = new TrieST();
+
+            prefixes.Put("abcd", 'a');
+            prefixes.Put("ab", 'a');
+            prefixes.Put("bcfx", 'b');
+            prefixes.Put("bcfd", 'b');
+
+            Console.WriteLine(prefixes.GetCount());
+
+            Console.ReadKey();
+        }
+    }
+}
