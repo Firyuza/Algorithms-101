0fccc4198a0f89ab3c5c501ba8405c94eb46af45
Firyuza <shigapovafz@gmail.com>
2015-10-22 17:57:19 +0300

    Assemble List Program

---
+++ AssembleList/AssembleList.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssembleList
+{
+    public class Node
+    {
+        public int Value { get; set; }
+
+        public Node Next { get; set; }
+    }
+    public class AssembleList
+    {
+        public static Node Assamble(Node list, int n)
+        {
+            Node p1 = list;
+            Node p2 = null;
+            var count = 1;
+
+            if (p1.Next != null)
+            {
+                p2 = p1.Next;
+            }
+
+            while (p2.Next != null)
+            {
+                p2 = p2.Next;
+                count++;
+
+                if (count > n)
+                {
+                    p1 = p1.Next;
+                    count--;
+                }
+            }
+
+            p2.Next = list;
+            var root = p1.Next;
+            p1.Next = null;
+
+            return root;
+        } 
+    }
+}
---
+++ AssembleList/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AssembleList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var list = new Node
+            {
+                Value = 1,
+                Next = new Node
+                {
+                    Value = 2,
+                    Next = new Node
+                    {
+                        Value = 3,
+                        Next = new Node
+                        {
+                            Value = 4,
+                            Next = new Node
+                            {
+                                Value = 5,
+                                Next = new Node
+                                {
+                                    Value = 6,
+                                    Next = null
+                                }
+                            }
+                        }
+                    }
+                }
+            };
+
+            var result = AssembleList.Assamble(list, 2);
+
+            var root = result;
+
+            while (root != null)
+            {
+                Console.Write(root.Value + " -> ");
+                root = root.Next;
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
