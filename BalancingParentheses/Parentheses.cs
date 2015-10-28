04c59a0617ebb720e01e1638a403381be068b4f9
Firyuza Shigapova <shigapovafz@gmail.com>
2015-10-29 01:05:29 +0300

    BalancingParentheses program

---
+++ BalancingParentheses/Parentheses.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace BalancingParentheses
+{
+    public class Parentheses
+    {
+        public static Stack<char> stack { get; set; }
+
+        public static void Balance(string str)
+        {
+            var result = "";
+            stack = new Stack<char>();
+
+            for (int i = 0; i < str.Length; i++)
+            {
+                if(str[i] == '(')
+                {
+                    stack.Push('(');
+                }
+                else
+                {
+                    if (stack.Count != 0)
+                    {
+                        result += stack.Pop() + ")";
+                    }
+                }
+            }
+
+            Console.WriteLine(result);
+        }
+    }
+}
