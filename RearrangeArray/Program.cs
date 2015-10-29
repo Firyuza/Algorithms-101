8989192c832aab0ad44aa9fcf9702ec4fe4df454
Firyuza <shigapovafz@gmail.com>
2015-10-29 14:37:00 +0300

    Rearrange array example

---
+++ RearrangeArray/Program.cs
@@ -0,0 +1,33 @@
+﻿/*
+ * Google interview
+
+Rearrange characters in a string so that no character repeats twice in a raw.
+
+Input: aaabc
+Output: abaca
+
+Input: aa
+Output: No valid output
+
+Input: aaaabc
+Output: No valid output
+ */
+using System;
+namespace RearrangeArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Entert string");
+
+            var str = Console.ReadLine();
+
+            Console.WriteLine("Result:");
+
+            Console.WriteLine(Rearrange.ReArray(str));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ RearrangeArray/Rearrange.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RearrangeArray
+{
+    public class Rearrange
+    {
+        public static string ReArray(string s)
+        {
+            var len = s.Length;
+            var map = new Dictionary<char, int>();
+            var lastSymbol = s[0];
+            map.Add(s[0], 0);
+            string result = s[0] + "";
+
+            for (var i = 1; i < len; i++)
+            {
+                if (s[i] != lastSymbol)
+                {
+                    result += s[i];
+
+                    if (map[lastSymbol] != 0)
+                    {
+                        result += lastSymbol;
+                        map[lastSymbol]--;
+                    }
+                    else
+                    {
+                        lastSymbol = s[i];
+                        map.Add(lastSymbol, 0);
+                    }
+                }
+                else
+                {
+                    map[lastSymbol]++;
+                }
+            }
+
+            if (result.Length != len)
+            {
+                return "No valid output";
+            }
+
+            return result;
+        }
+    }
+}
