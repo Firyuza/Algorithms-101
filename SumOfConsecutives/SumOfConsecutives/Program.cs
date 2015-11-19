6709d8f3f8733fe2c9cbfeaba8b1fb4a8b3bebee
Firyuza <shigapovafz@gmail.com>
2015-11-19 14:37:36 +0300

    Program SumOfConsecutives

---
+++ SumOfConsecutives/SumOfConsecutives/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace SumOfConsecutives
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = {1, 3, 5, 7, 9};
+
+            TargetSum Sum = new TargetSum();
+            
+            Sum.FindTargerConseq(a, 2);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SumOfConsecutives/SumOfConsecutives/TargetSum.cs
@@ -0,0 +1,73 @@
+﻿using System;
+namespace SumOfConsecutives
+{
+    public class TargetSum
+    {
+        private int[] Array;
+
+        private int Count;
+
+        private int Target;
+
+        public void FindTargerConseq(int[] a, int t)
+        {
+            Array = a;
+            Count = a.Length;
+            Target = t;
+
+            Find();
+        }
+
+        private void Find()
+        {
+            var id = Count - 1;
+            var find = false;
+            var curId = id;
+
+            var sum = Array[id];
+            id--;
+            if (sum != Target)
+            {
+                while (id >= 0 && !find)
+                {
+                    if (Array[id] + sum < Target)
+                    {
+                        sum += Array[id];
+                        id--;
+                    }
+                    else
+                    {
+                        if (Array[id] + sum == Target)
+                        {
+                            find = true;
+                        }
+                        else
+                        {
+                            curId--;
+                            sum = Array[curId];
+                            id = curId - 1;
+                        }
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine("Output: " + Array[id + 1]);
+                return;
+            }
+
+            if (find)
+            {
+                Console.WriteLine("Output:");
+                for (var i = id; i <= curId; i++)
+                {
+                    Console.Write(Array[i] + " ,");
+                }
+            }
+            else
+            {
+                Console.WriteLine("Output: nothing");
+            }
+        }
+    }
+}
